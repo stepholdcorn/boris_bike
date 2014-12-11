d709b8418a2b8c29f5ccb12e6313d9225b685e6c
stepholdcorn <stephieoldcorn@gmail.com>
2014-12-11 19:33:34 +0000

    Refactor user spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,15 +4,15 @@ describe User do
 
 	let(:user) {User.new}
 	let(:bike) {double :bike}
+	let(:station) {double :station}
 	
 	it "should not have a bike" do
 		expect(user).not_to have_bike
 	end
 
 	it "can rent a bike from a station" do
-		old_street = double :station
-		expect(old_street).to receive(:release_a_bike)
-		user.rent_bike_from(old_street)
+		expect(station).to receive(:release_a_bike)
+		user.rent_bike_from(station)
 	end
 
 	it "has the bike after renting it" do
@@ -26,9 +26,8 @@ describe User do
 	end
 
 	it "should return the bike to a station" do
-		old_street = double :station
-		expect(old_street).to receive(:dock_a_bike)
-		user.return_bike_to(old_street)
+		expect(station).to receive(:dock_a_bike)
+		user.return_bike_to(station)
 	end
 
 	it "should not have a bike after returning it" do
