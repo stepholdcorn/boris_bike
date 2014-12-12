03bfa6ada0d50b23966801b067b9e02fdb158115
stepholdcorn <stephieoldcorn@gmail.com>
2014-12-12 10:03:31 +0000

    Fixing the Van spec

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -9,12 +9,6 @@ describe Van do
 		expect(van.capacity).to eq(10)
 	end
 
-	# it "should move a bike when it's fixed" do
-	# 	bike.fix!
-	# 	bike.move!
-	# 	expect(bike).to be_moved
-	# end
-
 	it "should move a bike when it is broken" do
 		expect(bike).to receive(:move)
 		van.move_broken(bike)
